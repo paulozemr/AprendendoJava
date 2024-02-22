c777ffbe9d5a540814270ec7788498197d8d32bd
Windows 11 <Windows 11@DESKTOP-62PE5IS>
2024-02-22 02:19:04 -0300

    aaa

---
+++ src/ClassAprendendo.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class ClassAprendendo {
+
+	public static void main(String[] args) {
+		tartaruga a = new tartaruga();
+		a.fazerCoisas();	
+	}
+
+}
+
---
+++ src/tartaruga.java
@@ -0,0 +1,16 @@
+
+public class tartaruga {
+	
+	public static String SenhorTartaruga()  {
+		return "ola senhor tartaruga";
+	}
+	public static void fazerCoisas(){
+		int a = 5;		
+		while(a < 50) {
+			System.out.println("hi!");
+			a++;
+		}
+	}
+}
+	
+
